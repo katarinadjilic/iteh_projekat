8fcb9b6b35b40c71b44df9bdb72d95a67721c7a0
Katarina <katarinadjilic@gmail.com>
2020-01-04 13:34:44 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Grammy glasanje</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Merriweather:300,400|Montserrat:400,700" rel="stylesheet">
+	
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<script src="js/respond.min.js"></script>
+
+	</head>
+	<body>
+		
+	<div class="gtco-loader"></div>
+	
+	<div id="page">
+
+		<nav class="gtco-nav" role="navigation">
+			<div class="gtco-container">
+				
+				<div class="row">
+					<div class="col-sm-2 col-xs-12">
+						<div id="gtco-logo"><a href="index.php"><img src="images/logo.png" class="img img-responsive"> </div>
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="index.php">Home</a></li>
+							<li><a href="about.php">O nama</a></li>
+
+							<li><a href="glasaj.php">Glasajte</a></li>
+							<li><a href="login.php">Login</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</nav>
+
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="owl-carousel owl-carousel-fullwidth">
+						<div class="item">
+							<a href="#">
+								<img src="images/1.jpg" alt="Grammy prva slika">
+								<div class="slider-copy">
+									<h2>Grammy awards 2019</h2>
+								</div>
+							</a>
+						</div>
+						<div class="item">
+							<a href="#">
+								<img src="images/2.jpg" alt="Grammy druga slika">
+								<div class="slider-copy">
+									<h2>Grammy awards 2019</h2>
+								</div>
+							</a>
+						</div>
+						<div class="item">
+							<a href="#">
+								<img src="images/3.jpg" alt="Grammy treca slika">
+								<div class="slider-copy">
+									<h2>Grammy awards 2019</h2>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="gtco-section">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 gtco-heading text-center">
+						<h2>Dobro nam dosli</h2>
+						<p>Ovo je fan sajt gde mozemo da glasamo za grammy awards 2020. godine. Izaberimo nase pobednike i pruzimo im podrsku.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<footer id="gtco-footer" class="gtco-section" role="contentinfo">
+			<div class="gtco-copyright">
+				<div class="gtco-container">
+					<div class="row">
+						<div class="col-md-6 text-left">
+							<p><small>&copy; 2020 Grammy Fan Awards </small></p>
+						</div>
+						<div class="col-md-6 text-right">
+							<p><small>Designed by K-I-A </small></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
