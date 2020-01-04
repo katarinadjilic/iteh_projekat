096fb33e7fb340b1c0add5ba3d4a15f799798365
Katarina <katarinadjilic@gmail.com>
2020-01-04 15:02:16 +0100

    o nama stranica

---
+++ about.php
@@ -0,0 +1,109 @@
+<?php
+include "autoload.php";
+?>
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Grammy glasanje</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Merriweather:300,400|Montserrat:400,700" rel="stylesheet">
+	
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<script src="js/respond.min.js"></script>
+
+	</head>
+	<body>
+		
+	<div class="gtco-loader"></div>
+	
+	<div id="page">
+
+		<nav class="gtco-nav" role="navigation">
+			<div class="gtco-container">
+
+				<div class="row">
+					<div class="col-sm-2 col-xs-12">
+						<div id="gtco-logo"><a href="index.php"><img src="images/logo.png" class="img img-responsive"> </div>
+					</div>
+					<?php include 'meni.php' ?>
+				</div>
+
+			</div>
+		</nav>
+
+		<div class="gtco-section">
+			<div class="gtco-container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 gtco-heading text-center">
+						<h2>Nesto malo o nama</h2>
+						<p>Mi smo grupa studenata sa FON-a koja je zelela da omoguci svim fanovima GRAMMY nagrada da glasaju za svoje favorite. Rezultate glasanja cete moci da vidite u bilo kom trenutku</p>
+					</div>
+				</div>
+			</div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h2>Katarina Ilic</h2>
+                        <img class="img-responsive" src="images/katarina.jpg">
+                        <p>Student 4. godine ISIT-a</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>Irena Ilic</h2>
+                        <img class="img-responsive" src="images/irena.jpg">
+                        <p>Student 4. godine ISIT-a</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>Ana Jankovic</h2>
+                        <img class="img-responsive" src="images/ana.jpg">
+                        <p>Student 4. godine ISIT-a</p>
+                    </div>
+                </div>
+            </div>
+
+		</div>
+
+        <?php include 'futer.php'; ?>
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
